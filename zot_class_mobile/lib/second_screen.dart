3680c4e6cae81ccdecfff98e9c85922a08bc2e33
IleanaEscobar <izescobar5@gmail.com>
2022-11-05 20:31:45 -0700

    finish formatting info

--- zot_class_mobile/lib/second_screen.dart
+++ zot_class_mobile/lib/second_screen.dart
@@ -27,8 +27,7 @@ class _CoursesPageState extends State<CoursesPage> {
   List<String> course = <String>[];
   List<List<String>> code = <List<String>>[];
   List<List<String>> instructors = <List<String>>[];
-  List<String> units = <String>['54321','55555','12345', '129381','444444','111111','222222','333333','4444444','555555',
-  '333333','099999'];
+  List<List<String>> units = <List<String>>[];
   List<String> meetings = <String>['ICS 45', 'Writing 60', 'Math 2B', 'ICS 60','EECS 31',
   'Writing 39B','Math 2D', 'CompSci 161', 'BME 12', 'BioSci 90', 'Math 2A', 'Writing 50'];
 
@@ -44,14 +43,14 @@ class _CoursesPageState extends State<CoursesPage> {
       url += "&courseTitle=${courseName}";
     }
 
-    print(url);
     var response = http.get(Uri.parse(url));
     response.then((value) {
-      print(jsonDecode(value.body));
       var courses = [];
       var temp = [];
       var codeTemp = [];
       var instructorsTemp = [];
+      var unitTemp = [];
+      List<List<String>> unit = [];
       List<List<String>> codes = [];
       List<List<String>> instructorsList = [];
       for (int i = 0; i < jsonDecode(value.body).length; i++) {
@@ -59,26 +58,22 @@ class _CoursesPageState extends State<CoursesPage> {
         temp.add(jsonDecode(value.body)[i]['sections']);
         for (int e = 0; e <jsonDecode(value.body)[i]['sections'].length; e++) {
           codeTemp.add(jsonDecode(value.body)[i]['sections'][e]['sectionCode']);
+          unitTemp.add(jsonDecode(value.body)[i]['sections'][e]['units']);
           instructorsTemp.add(jsonDecode(value.body)[i]['sections'][e]['instructors'][0]);
         }
         codes.add(List<String>.from(codeTemp));
         instructorsList.add(List<String>.from(instructorsTemp));
+        unit.add(List<String>.from(unitTemp));
+        unitTemp = [];
         codeTemp = [];
         instructorsTemp = [];
       }
-      print(codes);
-      print(instructorsList);
       setState(() {
       course = List<String>.from(courses);
       code = codes;
       instructors = instructorsList;
-      print(instructors);
+      units = unit;
       });
-      // Do things
-      // List<dynamic> temp = jsonDecode(value.body)['json_list'];
-      // setState(() {
-      // depList = List<String>.from(temp);
-      // });
     });
    }
 
@@ -116,11 +111,10 @@ class _CoursesPageState extends State<CoursesPage> {
                 padding: const EdgeInsets.all(10),
                 itemCount: course.length,
                 itemBuilder: (BuildContext context, int index) {
-                  String codeString = "Code:  Instructors:\n";
+                  String codeString = "";
                   for(int n = 0; n < code[index].length; n++) {
-                    codeString += "${code[index][n]}   ${instructors[index][n]}\n";
+                    codeString += "Code:${code[index][n]}   Instructors:${instructors[index][n]}   Units:${units[index][n]}\n";
                   }
-                  print(codeString);
                   return Container(
                     height: 100,
                     color: Colors.blue,
@@ -138,12 +132,6 @@ class _CoursesPageState extends State<CoursesPage> {
                               fontSize: 20,
                             ),
                             ),
-                            // Text('Code: ${code[index]}',
-                            //     style: const TextStyle(
-                            //     color: Colors.black,
-                            //     fontWeight: FontWeight.bold,
-                            //     ),
-                            // ),
                           ],
                         ),
                   
@@ -157,7 +145,6 @@ class _CoursesPageState extends State<CoursesPage> {
                             showDialog(context: context, builder: (BuildContext context){
                               return AlertDialog(
                                 title: const Text('Sections'),
-                                // ode[index].forEach((String)"")
                                 content: Text(codeString),
                                 actions: <Widget>[
                                   TextButton(
